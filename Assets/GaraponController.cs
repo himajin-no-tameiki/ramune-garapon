0384fd2e05d84b691786a785b72e853b2e84aff3
himajin <kubotaryou.ryoukubota+himajin@gmail.com>
2018-09-24 17:19:06 +0900

    inital commit

---
+++ Assets/GaraponController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GaraponController : MonoBehaviour {
+	public GameObject rotateCenter;
+	public float rotateSpeedMultplier = 1f;
+	public float maxRotateSpeed = 40f;
+
+	private Quaternion startRotation;
+	private bool mouseDown;
+	private Camera cam;
+	private float angle = 0f;
+	private Rigidbody rb;
+
+	void Start () {
+		startRotation = transform.rotation;
+		startRotation = transform.localRotation;
+		cam = Camera.main;
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {	
+		if (mouseDown) {
+			var mouse = Input.mousePosition;
+			var rotateCenterPosition = cam.WorldToScreenPoint(rotateCenter.transform.position);
+			var delta = mouse - rotateCenterPosition;
+			var targetAngle = Mathf.Atan2(delta.x, delta.y) * Mathf.Rad2Deg;
+
+			var maxAngleDelta = Mathf.Min(rotateSpeedMultplier * Time.deltaTime, maxRotateSpeed * Time.deltaTime);
+			var nextAngle = Mathf.LerpAngle(angle, targetAngle, maxAngleDelta);
+
+			var nextRotation = Quaternion.AngleAxis(nextAngle, Vector3.forward) * startRotation;
+			rb.MoveRotation(nextRotation);
+
+			angle = nextAngle;
+		}
+	}
+
+	void OnMouseDown() {
+		mouseDown = true;
+		Debug.Log("down");
+  }
+
+	void OnMouseUp() {
+		mouseDown = false;
+		Debug.Log("up");
+	}
+}
---
+++ Assets/HandleController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleController : MonoBehaviour {
+
+	public GameObject objectToRotate;
+	public GameObject rotateCenter;
+	public float rotateSpeed = 40f;
+
+	private Quaternion startRotation;
+	private bool mouseDown;
+	private Camera cam;
+	
+	// Use this for initialization
+	void Start () {
+		startRotation = objectToRotate.transform.localRotation;
+		cam = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+
+}
